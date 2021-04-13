202e004531dce266ecb5d617e683fa92268b0f67
Christopher Allène <christopher.allene@mobsuccess.com>
2021-04-13 18:27:42 +0000

    First commit

---
+++ action.js
@@ -0,0 +1,21 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+//const getOctokit = require("./lib/actions/octokit");
+const { validatePR } = require("./lib/actions/pullRequest");
+
+exports.getActionParameters = function getActionParameters() {
+  const pullRequest = github.context.payload.pull_request;
+  const action = core.getInput("action", { required: true });
+  return { pullRequest, action };
+};
+
+exports.action = async function action() {
+  const { pullRequest, action } = exports.getActionParameters();
+
+  console.info(`Calling action ${action}`);
+  switch (action) {
+    case "validate-pr":
+      await validatePR({ pullRequest });
+      break;
+  }
+};
