b7292b8ecf5af2a37a58024ca80878de5f73141b
Christopher Allène <66381046+callms@users.noreply.github.com>
2023-08-03 10:34:51 +0200

    Add support for running when a comment is made (#66)

--- action.js
+++ action.js
@@ -5,22 +5,27 @@ const { onPRMerged } = require("./lib/actions/pullRequestMerged");
 const { generateChangelog } = require("./lib/actions/changelog");
 
 exports.getActionParameters = function getActionParameters() {
+  const comment = github.context.payload.comment;
+  const issue = github.context.payload.issue;
   const repository = github.context.payload.repository;
   const ref = github.context.ref;
   const pullRequest = github.context.payload.pull_request;
   const mergeGroup = github.context.payload.merge_group;
   const action = core.getInput("action", { required: true });
-  return { repository, ref, pullRequest, action, mergeGroup };
+  return { repository, ref, pullRequest, action, mergeGroup, issue, comment };
 };
 
 exports.action = async function action() {
+  const parameters = exports.getActionParameters();
+  console.log("Actiona prameters", JSON.stringify(parameters));
   const {
     repository,
     ref,
     pullRequest,
     action,
     mergeGroup,
-  } = exports.getActionParameters();
+    issue,
+  } = parameters;
 
   console.info(`Calling action ${action}`);
   switch (action) {
@@ -29,7 +34,7 @@ exports.action = async function action() {
         console.log("Ignoring PR validation when running in merge group");
         return;
       }
-      await validatePR({ pullRequest });
+      await validatePR({ pullRequest, issue });
       break;
     case "after-pr-merged":
       await onPRMerged({ pullRequest });
